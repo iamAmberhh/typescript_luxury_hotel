5809f6070ab476ab796c67e66b8488d449d60dab
iamAmberhh <melody19941231@gmail.com>
2024-01-01 10:48:13 +0800

    feat: modify NavbarComponent display

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,30 +1,74 @@
 <template>
-   <nav class="navbar navbar-expand-lg" style="height: 120px;">
+   <nav class="navbar navbar-dark navbar-expand-lg" style="height: 120px;">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/typescript-hotel/%E6%A1%8C%E6%A9%9F%E7%89%88/logo.png?raw=true" alt="logo" class="img-fluid logo"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <RouterLink
+          to="/"  
+          class="navbar-brand"
+        >
+          <img src="@/assets/img/logo.png" alt="logo" class="img-fluid logo">
+        </RouterLink>
+        <button type="button" class="navbar-toggler" data-bs-toggle="modal" data-bs-target="#mobileNavLink">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="d-none d-lg-flex" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active link-light" aria-current="page" href="#">客房旅宿</a>
+            <li class="nav-item me-lg-3">
+              <a class="nav-link active link-light p-3" aria-current="page" href="#">客房旅宿</a>
+            </li>
+            <li class="nav-item me-lg-3">
+              <RouterLink
+                to="/login"
+                class="nav-link link-light p-3"
+              >
+                會員登入
+              </RouterLink>
             </li>
             <li class="nav-item">
-              <a class="nav-link link-light" href="#">會員登入</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link btn btn-primary" href="#">立即訂房</a>
+              <a class="link-light btn btn-primary p-3 px-4 link-white" href="#">立即訂房</a>
             </li>
           </ul>
         </div>
+        <div class="d-lg-none modal fade" id="mobileNavLink" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+          <div class="modal-content">
+            <div class="modal-header bg-dark p-4 border-bottom-0">
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body bg-dark">
+              <ul class="h-100 d-flex flex-column justify-content-center align-items-center text-center">
+                <li class="mb-3">
+                  <a class="nav-link active link-light p-3" aria-current="page" href="#">客房旅宿</a>
+                </li>
+                <li class="mb-3">
+                  <RouterLink
+                    to="/login"
+                    class="nav-link link-light p-3"
+                  >
+                    會員登入
+                  </RouterLink>
+                </li>
+                <li class="w-100">
+                  <a class="d-block link-light btn btn-primary p-3 px-4 link-white" href="#">立即訂房</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     </nav>
 </template>
 
 <script setup lang="ts">
+// import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 
+// const mobileNavLink = ref(false)
+
+// const handleNavbarOpen = () => {
+//   mobileNavLink.value = true
+//   console.log(mobileNavLink.value)
+// }
 </script>
 
 
