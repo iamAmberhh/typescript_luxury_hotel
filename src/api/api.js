c42ec3c4186a35b42f1e58f0285deb9547406b49
iamAmberhh <melody19941231@gmail.com>
2024-01-20 11:47:01 +0800

    feat: add vite proxy config

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
+// const { VITE_APP_API } = import.meta.env;
 
 const service = axios.create()
 
 export default function axiosReq(config) {
   return service({
-    // baseURL: 'https://freyja-1d2y.onrender.com/api/v1',
-    baseURL: 'http://localhost:3005/api/v1',
+    baseURL: `https://freyja-1d2y.onrender.com/api/v1`,
+    // baseURL: 'http://localhost:3005/api/v1',
     timeout: 8000,
     ...config
   })
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
-// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as path from "path";
+// import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+  ? '/typescript_luxury_hotel/' : '/',
   plugins: [
     vue(),
   ],
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "src"),
+  //   },
+  // },
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://freyja-1d2y.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
